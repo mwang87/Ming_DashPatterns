d963ea03ccffd7661b3219725ce25a8ec35fa7df
Ming Wang <mwang87@gmail.com>
2021-08-04 18:07:41 -0700

    Update MSSpectrumDisplay.py

--- MSSpectrumDisplay.py
+++ MSSpectrumDisplay.py
@@ -5,11 +5,11 @@ ints = [peak[1]/max_int for peak in ms1_peaks]
 neg_ints = [intensity * -1 for intensity in ints]
 
 # Hover data
-hover_labels = ["{} m/z, {} int".format(mzs[i], ints[i]) for i in range(len(mzs))]
+hover_labels = ["{:.4f} m/z, {:.2f} int".format(mzs[i], ints[i]) for i in range(len(mzs))]
 
 ms1_fig = go.Figure(
     data=go.Scatter(x=mzs, y=ints, 
-        mode='markers+text',
+        mode='markers',
         marker=dict(size=0.00001),
         error_y=dict(
             symmetric=False,
